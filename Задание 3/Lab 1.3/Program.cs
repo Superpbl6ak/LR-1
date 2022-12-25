4d2a796007a0fd95d5827ebb44fad93c83bddad1
Superpbl6ak <114976892+Superpbl6ak@users.noreply.github.com>
2022-12-25 22:15:39 +0700

    Add files via upload

---
+++ Задание 3/Lab 1.3/Program.cs
@@ -0,0 +1,28 @@
+﻿Write("Введите min: ");
+int.TryParse(ReadLine(), out int min);
+Write("Введите max: ");
+int.TryParse(ReadLine(), out int max);
+
+int[,] mass = new int[5,5];
+
+Random rand = new Random();
+
+for (int i = 0; i < 5; i++)
+{
+    for (int x = 0; x < 5; x++)
+    {
+        mass[i, x] = rand.Next(min, max+1);
+    }
+}
+WriteLine("Матрица: ");
+for (int i = 0; i < 5; i++)
+{
+    for (int x = 0; x < 5; x++)
+    {
+        Write(mass[i, x]+" ");
+    }
+
+    WriteLine();
+}
+WriteLine("Для завершения программы нажмите любую клавишу...");
+ReadKey();
