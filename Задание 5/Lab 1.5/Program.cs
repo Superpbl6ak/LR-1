d8173ba2cdc950aed7905a75539c13c2e7852eb8
Superpbl6ak <114976892+Superpbl6ak@users.noreply.github.com>
2022-12-25 22:17:45 +0700

    Add files via upload

---
+++ Задание 5/Lab 1.5/Program.cs
@@ -0,0 +1,24 @@
+﻿Write("Введите число месяца: ");
+int.TryParse(ReadLine(), out int mes);
+if (mes>=1 && mes <=12)
+{
+    switch (mes)
+    {
+        case 12: WriteLine("Это зима");
+            break;
+        case >=9:
+            WriteLine("Это осень");
+            break;
+        case >=6:
+            WriteLine("Это лето");
+            break;
+        case >=3:
+            WriteLine("Это весна");
+            break;
+        case >=1:
+            WriteLine("Это зима");
+            break;
+    }
+
+}
+else{WriteLine("Такого месяца нет");}
